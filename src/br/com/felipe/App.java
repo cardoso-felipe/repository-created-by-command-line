a8da2c85e007007fa8bf7ad58bbfd1e68c3b50eb
Felipe Cardoso <felipecardososousa@gmail.com>
2023-02-18 17:52:41 -0300

    first commit

---
+++ src/br/com/felipe/App.java
@@ -0,0 +1,16 @@
+package br.com.felipe;
+
+import br.com.felipe.model.Cat;
+
+public class App {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        
+        System.out.println(cat);
+
+        /*int a = 10;
+        int b = 5;
+        System.out.println("Hello, World!" + (a+b));*/
+    }
+}
---
+++ src/br/com/felipe/model/Cat.java
@@ -0,0 +1,94 @@
+package br.com.felipe.model;
+
+import java.util.Date;
+
+public class Cat {
+
+    private String name;
+    private String breed;
+    private Date birthDate;
+
+    public Cat() {
+    }
+
+    public Cat(String name, String breed, Date birthDate) {
+        this.name = name;
+        this.breed = breed;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((breed == null) ? 0 : breed.hashCode());
+        result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cat other = (Cat) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (breed == null) {
+            if (other.breed != null)
+                return false;
+        } else if (!breed.equals(other.breed))
+            return false;
+        if (birthDate == null) {
+            if (other.birthDate != null)
+                return false;
+        } else if (!birthDate.equals(other.birthDate))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat [name=" + name + ", breed=" + breed + ", birthDate=" + birthDate + "]";
+    }
+
+    /*
+     * public Cat(String name, String breed, Date birthDate) {
+     * this.name = name;
+     * this.breed = breed;
+     * this.birthDate = birthDate;
+     * }
+     */
+
+}
